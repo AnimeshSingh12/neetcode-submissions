f46ce04a4e1f6d5a6c74cadba9acd907a01b1733
AnimeshSingh12 <80142956+AnimeshSingh12@users.noreply.github.com>
2026-08-13 23:30:38 +0530

    Update: buy-and-sell-crypto - submission-0

--- Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -1,18 +1,24 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int l = 0, r = 1;
-        int maxP = 0;
+        int maxProfit =0 ;
+        int l = 0 ;
+        int r = 1;
 
-        while (r < prices.size()) {
-            if (prices[l] < prices[r]) {
-                int profit = prices[r] - prices[l];
-                maxP = max(maxP, profit);
-            } else {
-                l = r;
+        while(r<prices.size())
+        {
+            if(prices[l]<prices[r])
+            {
+                int profit = prices[r]-prices[l];
+                maxProfit = max(maxProfit,profit);
+            }
+            else
+            {
+                l=r;
             }
             r++;
         }
-        return maxP;
+        return maxProfit;
+
     }
 };
